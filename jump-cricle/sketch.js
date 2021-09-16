faa7177b11acfa26bf9d2d96b15ab7049405280c
Jannek <jannekNY@gmail.com>
2021-09-16 15:08:37 +0200

    hoppe circle

---
+++ jump-cricle/sketch.js
@@ -0,0 +1,20 @@
+let XD = 69
+let VS = 150
+
+
+function setup() {
+  fill(52, 219, 235)
+  createCanvas(400, 400);
+}
+
+
+function draw() {
+  background(220);
+
+
+  circle(VS,200,XD);
+  
+  VS = VS+1
+
+
+}
