9e276fe38ce35c498e4d2411313d835309505da2
Jannek <jannekNY@gmail.com>
2021-10-08 07:36:51 +0200

    opgave 1 er lavet

---
+++ loop/sketch.js
@@ -0,0 +1,21 @@
+
+function setup() {
+  createCanvas(1000, 400);
+
+  textSize(30)
+  text('Du skal åbne konsollen (Højre klick -> Undersøg -> Console)', 10, 50)
+
+
+
+  //Opgave 1
+  a = 10
+  while (a > 0) {
+
+    console.log("Starter" + a)
+
+    a = a - 1
+  }
+
+  //Opgave 2
+  
+}
