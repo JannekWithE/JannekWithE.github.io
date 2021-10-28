6df7ed04eb00f362147fc451e752bab710328002
Jannek <jannekNY@gmail.com>
2021-10-28 13:54:47 +0200

    opgave punkt lavet

---
+++ arr/sketch.js
@@ -0,0 +1,11 @@
+
+function setup() {
+  createCanvas(1000, 400);
+
+  textSize(30)
+  text('Du skal åbne konsollen (Højre klick -> Undersøg -> Console)', 10, 50)
+
+
+
+  
+}
--- clickcirkel/sketch.js
+++ clickcirkel/sketch.js
@@ -1,10 +1,26 @@
+let ystart = 0
+let xstart = 0
+let yend = 0
+let xend = 0
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 function draw() {
   background(200);
-  
 
-  
+  strokeWeight(3)
+  stroke(mouseX, mouseY, 200)
 
+  if (mouseIsPressed) {
+    xend = mouseX
+    yend = mouseY
+  }
+
+  line(xstart, ystart, xend, yend)
+}
+
+function mousePressed() {
+  xstart = mouseX
+  ystart = mouseY
 }
