32aedca2c8164035efb3fbabc0f21680d93170ef
Jannek <jannekNY@gmail.com>
2021-10-29 09:19:09 +0200

    lidt på ogaven

--- arr/sketch.js
+++ arr/sketch.js
@@ -4,8 +4,17 @@ function setup() {
 
   textSize(30)
   text('Du skal åbne konsollen (Højre klick -> Undersøg -> Console)', 10, 50)
+  text('ᕕ( ᐛ )ᕗ', 10, 100)
+  text('__________________________________________________________________', 10, 120)
 
 
+  for (let x = 0; x <= 10; x++) {
 
-  
+    for (let y = 0; y <= 3; y++){
+
+      
+    }
+}
+
+
 }
