9ddc85075a07921ffe232c9f94d786d5010af574
Jannek <jannekNY@gmail.com>
2022-05-05 14:10:43 +0200

    Deride added

--- tegne/sketch.js
+++ tegne/sketch.js
@@ -1,10 +1,27 @@
+let firkantshape = []
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
 }
 function draw() {
-  background("lightblue"); 
+  background("lightblue");
+
+  //Dette er teskt så man ved havd er hvad
+  textSize(30)
+  text("4 = Firkant", 10, 30)
+
+  //
+  for (const Firkant of firkantshape) {
+    Firkant.render()
+  }
+  
+
+}
 
 
+function keyPressed() {
+  if (key == "c") {
+    reset()
+  }
 
 }
